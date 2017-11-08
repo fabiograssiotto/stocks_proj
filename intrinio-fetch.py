70cec60535550398d5fd2e6b81aa41951c266e06
Fabio Grassiotto <fabiogr@gmail.com>
2017-11-07 23:20:13 -0200

    Added more stocks

--- intrinio-fetch.py
+++ intrinio-fetch.py
@@ -1,15 +1,28 @@
 import intrinio
 
-intrinio.client.username = 'cbf3647ca29bd2236958b388b875b9fd'
-intrinio.client.password = '932441d13c084642d3bfece69acb1227'
+#intrinio.client.username = 'cbf3647ca29bd2236958b388b875b9fd'
+#intrinio.client.password = '932441d13c084642d3bfece69acb1227'
 
-df_appl = intrinio.news('AAPL')
-df_appl.to_csv('AAPL.csv')
-df_googl = intrinio.news('GOOGL')
-df_googl.to_csv('GOOGL.csv')
-df_msft = intrinio.news('MSFT')
-df_msft.to_csv('MSFT.csv')
-df_fb = intrinio.news('FB')
-df_fb.to_csv('FB.csv')
-df_tsm = intrinio.news('TSM')
-df_tsm.to_csv('TSM.csv')
+intrinio.client.username = '9ef28500250f001837e864630f98010d'
+intrinio.client.password = 'b6d38989841c00d252232a7ef1822f20'
+
+#df_appl = intrinio.news('AAPL')
+#df_appl.to_csv('AAPL.csv')
+#df_googl = intrinio.news('GOOGL')
+#df_googl.to_csv('GOOGL.csv')
+#df_msft = intrinio.news('MSFT')
+#df_msft.to_csv('MSFT.csv')
+#df_fb = intrinio.news('FB')
+#df_fb.to_csv('FB.csv')
+#df_tsm = intrinio.news('TSM')
+#df_tsm.to_csv('TSM.csv')
+#df_csco = intrinio.news('CSCO')
+#df_csco.to_csv('CSCO.csv')
+#df_intc = intrinio.news('INTC')
+#df_intc.to_csv('INTC.csv')
+#df_orcl = intrinio.news('ORCL')
+#df_orcl.to_csv('ORCL.csv')
+df_ibm = intrinio.news('IBM')
+df_ibm.to_csv('IBM.csv')
+df_sap = intrinio.news('SAP')
+df_sap.to_csv('SAP.csv')
