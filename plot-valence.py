0c577ac6fe685f9a5fef98499da1b30a9542a21d
Fabio Grassiotto <fabiogr@gmail.com>
2017-11-15 22:02:29 -0200

    updated code

--- plot-valence.py
+++ plot-valence.py
@@ -13,7 +13,9 @@ else:
 engine = create_engine('sqlite:///news.db')
 table_name = 'news_table'
 df = pd.read_sql_query('SELECT publication_date, valence FROM news_table WHERE ticker="{0}"'.format(sys.argv[1]), engine)
-df.set_index('publication_date')
+#df.set_index('publication_date')
+df['publication_date'] = pd.to_datetime(df['publication_date'])
+df.sort_values(by='publication_date')
 
-sns.tsplot([df.valence], color="indianred")
+plt.scatter(df['publication_date'].values, df['valence'].values, s =100, c = 'red')
 plt.show()
