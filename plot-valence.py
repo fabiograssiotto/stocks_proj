32bf7588a4f6ac654d6d9665f39bfd0d24c00ae8
Fabio Grassiotto <fabiogr@gmail.com>
2017-11-17 18:18:27 -0200

    Add files via upload

--- plot-valence.py
+++ plot-valence.py
@@ -5,31 +5,56 @@ import pandas as pd
 from sqlalchemy import create_engine
 import sys
 
-def get_graph_data(ticker):
+def get_valence_data(ticker):
     df = pd.read_sql_query('SELECT publication_date, valence, title FROM news_table WHERE ticker="{0}"'.format(ticker), engine)
     df['publication_date'] = pd.to_datetime(df['publication_date'])
     df['date_num'] = df.publication_date.values.astype('datetime64[s]').astype('int')
     df.sort_values(by='publication_date')
 
-    # Remove dados esparsos antes de 2016.
-    df = df[(df['publication_date'].dt.year > 2016)]
     # Agrega valências por dia
     return df.groupby([pd.Grouper(freq='D',key='publication_date')]).mean()
 
 engine = create_engine('sqlite:///news.db')
 
-fig = plt.figure()
-fig.suptitle('Valências de notícias de ações', fontsize=16)
+sns.set_style("white")
+fig, axs = plt.subplots(2,5)
+fig.suptitle('Valências de Notícias e Valores de Ações', fontsize=20)
 
-ticker_lst = ['AAPL', 'CSCO', 'FB', 'GOOGL', 'IBM', 'INTC', 'MSFT', 'ORCL', 'SAP', 'TSM']
-subplot_num = 1
+#ticker_lst = ['AAPL', 'CSCO', 'FB', 'GOOGL', 'IBM', 'INTC', 'MSFT', 'ORCL', 'SAP', 'TSM']
+ticker_lst = ['AAPL', 'CSCO', 'FB', 'IBM', 'INTC', 'MSFT', 'ORCL', 'SAP', 'TSM']
+
+subplot_line = 0
+subplot_col = 0
+
 for ticker in ticker_lst:
-    a = get_graph_data(ticker)
-    ax = fig.add_subplot(2, 5, subplot_num)
-    ax = sns.regplot(x='date_num', y='valence', data=a, order=1)
-    ax.set_title("Valência das noticías para {0}".format(ticker))
-    ax.set_xlabel('Data')
-    ax.set_ylabel('Valência')
-    subplot_num = subplot_num + 1
+    valence_data = get_valence_data(ticker)
+    ticker_data = pd.read_csv("nasdaqHistorical/{0}_Historical.csv".format(ticker))
+    ticker_data.date = pd.to_datetime(ticker_data.date)
+    ticker_data.sort_values(by='date')
+    ticker_data = ticker_data.set_index(ticker_data.date)
+
+    # Concatena dados e remove linhas sem informação
+    plot_df = pd.concat([valence_data, ticker_data], axis=1)
+    plot_df = plot_df.dropna()
+    plot_df = plot_df[['close', 'valence']]
+
+    plot_df['date'] = plot_df.index
+    plot_df['date_f'] = pd.factorize(plot_df['date'])[0] + 1
+    mapping = dict(zip(plot_df['date_f'], plot_df['date'].dt.date))
+    
+    ax1 = axs[subplot_line][subplot_col]
+    ax2 = ax1.twinx()
+    ax1.set_title(ticker)
+    ax1.set_ylabel('Valência')
+    ax1.set_xlabel('Data')
+    ax2.set_ylabel('Preço Ação')
+
+
+    sns.regplot(x=plot_df.date_f, y=plot_df.valence, data=plot_df, ax = ax1, color="b", order=1)
+    sns.tsplot(data=plot_df.close, time=plot_df.date_f, ax = ax2, color="r")
+    subplot_col = subplot_col + 1;
+    if (subplot_col > 4):
+        subplot_col = 0
+        subplot_line = 1
 
 plt.show()
