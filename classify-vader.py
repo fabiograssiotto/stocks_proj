ddeb5c511fe5003802cbdba0eff5741462c9b59a
Fabio Grassiotto <fabiogr@gmail.com>
2017-11-21 22:40:33 -0200

    Updated notebook with VADER data

---
+++ classify-vader.py
@@ -0,0 +1,21 @@
+# Script para executar classificação das manchetes relacionadas a ações.
+import pandas as pd
+from sqlalchemy import create_engine
+import vaderSentiment
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+# Test Data
+engine = create_engine('sqlite:///news.db')
+table_name = 'news_table'
+test_df = pd.read_sql_table('news_table', engine)
+
+vader = SentimentIntensityAnalyzer()
+
+test_df['valence'] = test_df.apply(lambda x: vader.polarity_scores(x['title'])['compound'], axis=1)
+
+# Output
+print(test_df['valence'].value_counts())
+
+# Escreve a classificação no banco de dados.
+test_df = test_df.drop('index', axis = 1)
+test_df.to_sql('news_table', engine, if_exists='replace')
